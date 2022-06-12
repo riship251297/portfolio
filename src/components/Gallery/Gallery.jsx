deba6f04564b38c723dd461739c29178879a1420
Rishikesh Phatangare <riship251297@gmail.com>
2022-06-12 16:03:54 -0400

    changes

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,10 +1,51 @@
-import React from 'react'
+import axios from 'axios'
+import {useState} from 'react'
 import Navbar from '../Navbar/Navbar'
 
-const Gallery = () => {
+function Gallery() 
+{
+  const [news, setNews] = useState([])
+
+  const fetchNews = () => {
+    axios.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=889a2b39120c4636ae66833b27dcac39")
+      .then((response) => {
+        console.log(response);
+        setNews(response.data.articles)
+      })
+  }
   return (
+    <>
     <Navbar/>
+    <div className="container my-3">
+        <div className="row">
+          <div className="col-4">
+            <button className='btn btn-primary' onClick={fetchNews}>FetchNews</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          {
+            news.map((value,index) => {
+              return (
+                <div key={index} className="col-4">
+                  <div className="card" style={{ width: "18rem" }}>
+                    <img src={value.urlToImage} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                      <h5 className="card-title">{value.title}</h5>
+                      <p className="card-text">{value.description}</p>
+                      <a href="#" className="btn btn-primary">Main</a>
+                    </div>
+                  </div>
+                </div>
+              );
+            })
+          }
+        </div>
+      </div>
+    </>
   )
 }
 
-export default Gallery
+export default Gallery;
