978339d728c83dd0f5388c6db6a470e2d806da8e
Rishikesh Phatangare <riship251297@gmail.com>
2022-08-23 16:20:03 -0400

    changes

--- src/components/Research/Research.jsx
+++ src/components/Research/Research.jsx
@@ -77,6 +77,10 @@ function Research()
                                             <h4 className="plp"> FSL BET generated images </h4>
                                             <h4 className="opol"> Face and brain tissue probability by model</h4>
                                         </div>
+                                        <div className="io">
+                                            <img src={"images/rishi_0.2.png"} className="img-circled"width="900px"height="400px"alt=''/>
+                                            <h4>0.8 and 0.3</h4>
+                                        </div>
                                         <div className="uo">
                                             <img src={"images/rishi_0.3.png"} className="img-circled"width="900px"height="400px"alt=''/>
                                             <h4>0.35 and 0.2</h4>
@@ -85,10 +89,7 @@ function Research()
                                             <img src={"images/rishi_0.4.png"} className="img-circled"width="900px"height="400px"alt=''/>   
                                             <h4>0.17 and 0.12</h4>
                                         </div>
-                                        <div className="io">
-                                            <img src={"images/rishi_0.2.png"} className="img-circled"width="900px"height="400px"alt=''/>
-                                            <h4>0.8 and 0.3</h4>
-                                        </div>
+                                      
                                     </div>
                                 </div>
                             </ol>
